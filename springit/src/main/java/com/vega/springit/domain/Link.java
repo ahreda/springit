fffa89c4bdd53fccc6089b5bd648cdde99d7a415
Ahmad Reda <ahmad.ah.reda@gmail.com>
2020-06-10 22:20:28 +0200

    Adding Entities and Refactored using Project Lombok

---
+++ springit/src/main/java/com/vega/springit/domain/Link.java
@@ -0,0 +1,23 @@
+package com.vega.springit.domain;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity
+@NoArgsConstructor
+@Data
+
+public class Link {
+    @Id
+    @GeneratedValue
+    private long id;
+    private String title;
+    private String url;
+}
+
+
