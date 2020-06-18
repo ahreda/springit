259aa11d2683421e61d1e9e4c2d63f51f85b6149
Ahmad Reda <ahmad.ah.reda@gmail.com>
2020-06-18 02:06:40 +0200

    Creating our controllers

--- springit/src/main/java/com/vega/springit/controllers/LinkController.java
+++ springit/src/main/java/com/vega/springit/controllers/LinkController.java
@@ -1,55 +1,18 @@
 package com.vega.springit.controllers;
 
-import com.vega.springit.domain.Link;
-import com.vega.springit.repository.LinkRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
 
-
-@RestController
+@Controller
 public class LinkController {
 
-    LinkRepository linkRepository;
+    @GetMapping("/foo")
+    public String foo(Model model){
 
-    public LinkController(LinkRepository linkRepository) {
-        this.linkRepository = linkRepository;
+        model.addAttribute("pageTitle","This page is foo");
+        return "foo";
     }
-
-
-   @GetMapping("/links")
-    public Optional<Link> getAllTopics() {
-        Optional<Link> myLinks = linkRepository.findById((long) 2);
-        return myLinks;
-
-        }
-
-
-    @PostMapping("/create/{title}")
-    public void create(@PathVariable String title){
-        Link link = new Link(title,"https://therealdanvega.com/spring-boot-2");
-        linkRepository.save(link);
-
-
-    }
-
-   /* @GetMapping("/{id}")
-    public Optional<Link> read(@PathVariable Long id) {
-        return linkRepository.findById(id);
-
-    }*/
-
-/*
-    @PutMapping("/{id}")
-    public Link update(@PathVariable Long id, @ModelAttribute Link link){
-        return linkRepository.save(link);
-    }*/
-  /*  @DeleteMapping("/{id}")
-    public void delete(@PathVariable Long id){
-        linkRepository.deleteById(id);
-
-    }*/
 
 }
