b0de9026d3fc2dd42a4f65cd95468d8eab887a7b
Ahmad Reda <ahmad.ah.reda@gmail.com>
2020-06-20 17:46:57 +0200

    Add methids to link in order to get the domain name of the link and method to get the date in form of " less than minutes/hour/day .."

--- springit/src/main/java/com/vega/springit/domain/Link.java
+++ springit/src/main/java/com/vega/springit/domain/Link.java
@@ -1,12 +1,19 @@
 package com.vega.springit.domain;
 
+import com.vega.springit.services.BeanUtil;
 import lombok.*;
+import org.ocpsoft.prettytime.PrettyTime;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Objects;
 
@@ -41,7 +48,20 @@ public class Link extends Auditable {
        comments.add(comment);
    }
 
+    public String getDomainName() throws URISyntaxException {
+        URI uri = new URI(this.url);
+        String domain = uri.getHost();
+        return domain.startsWith("www.") ? domain.substring(4) : domain;
+    }
 
+    public String getPrettyTime() {
+        PrettyTime pt = BeanUtil.getBean(PrettyTime.class);
+        return pt.format(convertToDateViaInstant(getCreationDate()));
+    }
+
+    private Date convertToDateViaInstant(LocalDateTime dateToConvert) {
+        return java.util.Date.from(dateToConvert.atZone(ZoneId.systemDefault()).toInstant());
+    }
 
 }
 
